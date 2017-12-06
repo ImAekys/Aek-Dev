5e505d91088cb8ff47840ff5ed9727baec0930e7
Aven <32174784+Aven678@users.noreply.github.com>
2017-12-06 18:46:52 +0100

    Creating a interface Command

--- src/man/java/fr/aekys/aekbotdev/commands/Command.java
+++ src/man/java/fr/aekys/aekbotdev/commands/Command.java
@@ -1,6 +1,6 @@
 package fr.aekys.aekbotdev.commands;
 
-import net.dv8tion.jda.core.events.MessageReceivedEvent;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
 public interface Command
 {
